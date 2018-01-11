ce17408a0d7bc016294b573e11bda3220b0b0ea9
DZG-PC\DZG <jsjjdzg67@gmail.com>
2018-01-11 14:29:11 +0800

    慢慢微调项目

--- src/main/java/com/bocloud/work/controller/HelloController.java
+++ src/main/java/com/bocloud/work/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.bocloud.work.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping(value = { "/hi", "hello" })
 	@ResponseBody
-	public String home() {
+	public String home(HttpServletRequest request) {
+		System.out.println(request.getLocalAddr());
 		return "Hello World!";
 	}
 
